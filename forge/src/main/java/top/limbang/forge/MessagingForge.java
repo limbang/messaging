c94a45c4ef2844a8c50c4996776989c4cb5b0c4f
limbang <495071565@qq.com>
2022-11-27 20:32:45 +0800

    feat: 初始化 1.18.2-forge-fabric-like 项目

---
+++ forge/src/main/java/top/limbang/forge/MessagingForge.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2022 limbang and contributors.
+ *
+ * 此源代码的使用受 GNU AGPLv3 许可证的约束，该许可证可在"LICENSE"文件中找到。
+ * Use of this source code is governed by the GNU AGPLv3 license that can be found in the "LICENSE" file.
+ */
+
+package top.limbang.forge;
+
+import dev.architectury.platform.forge.EventBuses;
+import top.limbang.Messaging;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Messaging.MOD_ID)
+public class MessagingForge {
+    public MessagingForge() {
+        // Submit our event bus to let architectury register our content on the right time
+        EventBuses.registerModEventBus(Messaging.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        Messaging.init();
+    }
+}
