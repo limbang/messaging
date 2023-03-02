975f9b22896d9b30f98b7505bbcd4b98b183c095
limbang <495071565@qq.com>
2023-03-03 00:08:54 +0800

    feat: 添加 fabric 的服务器启动、停止事件监听

--- fabric/src/main/java/top/limbang/fabric/MessagingFabric.java
+++ fabric/src/main/java/top/limbang/fabric/MessagingFabric.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 limbang and contributors.
+ * Copyright 2023 limbang and contributors.
  *
  * 此源代码的使用受 GNU AGPLv3 许可证的约束，该许可证可在"LICENSE"文件中找到。
  * Use of this source code is governed by the GNU AGPLv3 license that can be found in the "LICENSE" file.
@@ -7,12 +7,19 @@
 
 package top.limbang.fabric;
 
-import top.limbang.fabriclike.MessagingFabricLike;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import top.limbang.MessagingServer;
+import top.limbang.fabriclike.MessagingFabricLike;
 
 public class MessagingFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         MessagingFabricLike.init();
+
+        // 注册服务器启动完成事件监听
+        ServerLifecycleEvents.SERVER_STARTED.register(MessagingServer::create);
+        // 注册服务器停止事件监听
+        ServerLifecycleEvents.SERVER_STOPPED.register(server -> MessagingServer.stop());
     }
 }
